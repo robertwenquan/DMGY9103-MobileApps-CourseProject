e1300a7e4cd8d02e9d1e389e104146e824a25847
robertwenquan <robert.wen@nyu.edu>
2015-05-04 17:33:06 -0400

    add photo store class

---
+++ SnapIt/Snaplt/PhotoInspirationStore.h
@@ -0,0 +1,13 @@
+//
+//  PhotoInspirationStore.h
+//  Snaplt
+//
+//  Created by Robert Wen on 5/4/15.
+//  Copyright (c) 2015 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PhotoInspirationStore : NSObject
+
+@end
---
+++ SnapIt/Snaplt/PhotoInspirationStore.m
@@ -0,0 +1,13 @@
+//
+//  PhotoInspirationStore.m
+//  Snaplt
+//
+//  Created by Robert Wen on 5/4/15.
+//  Copyright (c) 2015 apple. All rights reserved.
+//
+
+#import "PhotoInspirationStore.h"
+
+@implementation PhotoInspirationStore
+
+@end
